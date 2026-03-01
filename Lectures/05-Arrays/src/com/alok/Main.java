0a62029078fcd4706ce3701e03616ac4ac679540
derekcoded <alokcodes21@gmail.com>
2026-03-01 16:28:48 +0530

    day7

---
+++ Lectures/05-Arrays/src/com/alok/Main.java
@@ -0,0 +1,9 @@
+package com.alok;
+
+public class Main {
+    public static void main(String[] args) {
+        //Q: store 5 roll no.
+        int[] rollno= new int[5];//same data type variable
+        //as index starts from 0 it will print 4
+    }
+}
