bf1a36858725780f7b33080fa3326754bd75c8f4
derekcoded <alokcodes21@gmail.com>
2026-03-04 20:31:45 +0530

    day11

---
+++ Lectures/05-Arrays/src/com/alok/question.java
@@ -0,0 +1,11 @@
+package com.alok;
+
+public class question {
+    public static void main(String[] args) {
+       // System.out.println("hello");
+        for (int i = 0; i <=5 ; i++) {
+            System.out.println("hello");
+
+        }
+    }
+}
