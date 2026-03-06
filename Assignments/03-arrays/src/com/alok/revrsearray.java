9001fea91bd95a54b1ea6653e7b0f65a649992e7
derekcoded <alokcodes21@gmail.com>
2026-03-06 17:20:40 +0530

    day13

--- Assignments/03-arrays/src/com/alok/revrsearray.java
+++ Assignments/03-arrays/src/com/alok/revrsearray.java
@@ -2,7 +2,7 @@ package com.alok;
 
 public class revrsearray {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world!");
     }
 
 }
